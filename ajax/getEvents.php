4a639c1b67c30f78ca93e57575e702267ef07caf
SAKSHI-GOYAL321 <sakshig077@gamil.com>
2022-02-25 16:06:20 +0530

    by Sakshi

--- ajax/getEvents.php
+++ ajax/getEvents.php
@@ -15,7 +15,7 @@ while($data_row=$query->fetch())
     $link = $data_row['link'];
 ?>
     <div class="events_row ">
-        <a href="#pop-up" onclick="popup($name, $date, $time, $desc, $link);"><div class="event">
+        <a href="#pop-up_<?php echo $data_row['event_id']; ?>"><div class="event">
                 <div class="date">
                     <p class="event_date"><?php echo $data_row['event_date'];?> </p>
                 </div>
@@ -25,22 +25,19 @@ while($data_row=$query->fetch())
                     <p class="event_time fa fa-clock-o"><?php echo $data_row['event_time'];?> </p>
                 </div>
         </a>
-        <div class="pop-up" id="pop-up">
+        <div class="pop-up" id="pop-up_<?php echo $data_row['event_id']; ?>" >
             <div class="pop-up__content">
             <a class="pop-up__close" href="#container">x</a>
             <h4><?php echo $data_row['event_name']; ?></h4>
             <p class="about_event_detail"><?php  echo $data_row['about_event']; ?></p>
             <p><?php echo $data_row['event_date']; ?></p>
             <p><?php echo $data_row['event_time']; ?></p>
-            <button class="btn-primary open-file-style" onclick="window.location.href = "<?php echo $data_row['link']; ?>">Click Here</button>
+            <button onclick=" window.location.href = <?php echo $data_row['link']; ?>" >Click Here</button>
             </div>
         </div>
     </div>
 <?php
 }
 
-function popup($name, $date, $time, $about, $link){
-    
-}
 
 ?>
