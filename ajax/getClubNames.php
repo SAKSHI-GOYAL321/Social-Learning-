5a384c9e7692df458b29b039965cc11820a0d464
SAKSHI-GOYAL321 <82642140+SAKSHI-GOYAL321@users.noreply.github.com>
2022-03-20 14:52:17 +0530

    add join button query

--- ajax/getClubNames.php
+++ ajax/getClubNames.php
@@ -77,7 +77,7 @@
  
     if(password_verify("ClubNames",$_POST['token'])){
         $email = $_SESSION['email'];
-        $query = $db->prepare("SELECT c.club_name, c.club_img, c.club_id, c.club_desc FROM clubname as c join clubmembers as m on c.club_id=m.club_id where m.email!=? AND c.club_admin_email!=?");
+        $query = $db->prepare('SELECT DISTINCT c.club_id, c.club_name, c.club_img, c.club_desc, c.club_admin_email from clubname as c LEFT JOIN clubmembers as m ON c.club_id = m.club_id where c.club_admin_email != ? AND (m.email IS NULL OR m.email != ?)');
         $data = array($email,$email);
         $execute = $query->execute($data); 
         while($datarow = $query->fetch())
@@ -92,22 +92,17 @@
                                         </div>
                                         <div class="btn-style-block">
                                             <a class="btn-1" href="./clubPage.php?club_id=<?php echo $datarow['club_id']?>"><button onclick="removeButton(<?php echo $datarow['club_id']; ?>)" class="width100 btn btn-primary">Join</button></a>
-                                            <!-- <button onclick="DeleteClub()"class=" btn-1 btn btn-primary">Delete</button> -->
                                         </div>
                                     </div>
                                 </div>
                                 <figure class="col-sm-8 paddoff hover-img">
                                     
                                     <img class="image" src="<?php echo $datarow['club_img'] ?>" alt="">
-                                
-                                <figcaption>
-                                    <p><?php echo $datarow['club_desc'] ?></p>
-                                </figcaption>
+                                    <figcaption>
+                                        <p><?php echo $datarow['club_desc'] ?></p>
+                                    </figcaption>
                               </figure>
-                    
-                            
                             </div>
-                           
                 </div>
         <?php
         }
