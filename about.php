aaa3b1d733752d5cc15bcfd6b17f755442622ca8
aditi944 <aditijha201200@gmail.com>
2022-04-04 20:32:28 +0530

    aditi

---
+++ about.php
@@ -0,0 +1,71 @@
+<?php
+session_start();
+?>   
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/normalize.min.css">
+    <!-- <link rel="stylesheet" href="css/dashboard.css"> -->
+    <link rel="stylesheet" href="css/dashboard.css"> 
+    <link rel="stylesheet" href="css/profile.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="Hamburger.js"></script>
+</head>
+<body>
+<section id="top-nav">
+        <div class="col-sm-12 paddoff">
+            <div class="topnav">    
+                <i class="fa fa-bars " onclick="show();"></i>
+                <a href="logout.php">Log out</a></i>    
+                <a href="profile_page.php"><?php echo $_SESSION['uname']?></a></i>
+            </div>
+        </div>
+    </section>
+    <section id="side-nav">
+    <div class="col-sm-12 paddoff">
+            <div class="contain">
+                <div class="col-sm-2">
+                    <div id="Ham" class="sidenav">
+                        <a href="#"><span style="padding: 4px;"><i class="fa fa-info"></i></span> About</a>
+                        <a href="dashboard.php"><span style="padding: 4px;"><i class="fa fa-home"></i></span>Dashboard</a>
+                        <a href="profile.php"><span style="padding: 4px;"><i class="fa fa-user-circle"></i></span>Profile</a>
+                        <a href="blog.php"><span style="padding: 4px;"><i class="fa fa-pencil"></i></span>Blog</a>
+                        <a href="resourse.php"><span style="padding: 4px;"><i class="fa fa-pencil"></i></span>Resourse</a>
+                        <a href="clubDash.php"><span style="padding: 4px;"><i class="fa fa-cog"></i></span>Clubs</a>
+                        <a href="#"><span style="padding: 4px;"><i class="fa fa-phone"></i></span>Contact</a>
+                    </div>
+                </div>
+
+            </div>
+    </div>
+</section>
+
+
+    
+
+<script type="text/javascript">
+        function show(){
+            var sidenav= document.getElementsByClassName("sidenav");
+            console.log("check1");
+            if(sidenav.style.display=="none"){
+                sidenav.classList.remove = "non-active";
+                sidenav.classList.add="active";
+            }
+            else{
+                sidenav.classList.remove = "active";
+                sidenav.classList.add="non-active";
+            }
+            // sidenav.classList.toggle("active");
+            console.log("check3");
+        } 
+    </script>
+
+</body>
+</html>
---
+++ ramdom.php
@@ -0,0 +1,22 @@
+<script src="ckeditor/ckeditor.js"></script>
+
+<form method="POST">
+<div class="margin">
+                                <label>Bio:</label>
+                                <input type="text" id="bio" name="bio" placeholder="Tell us about yourself"
+                                    class="form-control form-style" required>
+</div>
+<div>
+    <textarea id="editor" name="editor" placeholder="editor" >
+
+
+    </textarea>  
+    <input type="submit"/>Submit
+</div>
+
+
+</form>
+
+<script src="" >
+    CKEDITOR.replace("editor");
+</script>
