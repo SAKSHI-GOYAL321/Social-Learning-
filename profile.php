97500e5b876e70d16387dbbaca51f037727542ee
SAKSHI-GOYAL321 <82642140+SAKSHI-GOYAL321@users.noreply.github.com>
2022-05-06 18:14:29 +0530

    login restriction

--- profile.php
+++ profile.php
@@ -27,10 +27,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script> -->
-    <!-- <script src="Hamburger.js"></script> -->
 </head>
-
 <body>
     <section id="top-nav">
         <div class="col-sm-12 paddoff">
@@ -144,11 +141,6 @@
     </section>    
     <script type="text/javascript" src="Hamburger.js"></script>
     <script>
-//         $("#files").change(function() {
-//   filename = this.files[0].name;
-//   console.log(filename);
-// }); 
-
         $("#photo").change(function() {
             filename = this.photo[0].name;
             console.log(filename);
